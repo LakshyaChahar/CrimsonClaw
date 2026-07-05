161055e294d924402df4a8367c0aa916cebd5a2b
exharmonic <aryanbasra1138@gmail.com>
2026-07-05 14:22:39 +0530

    Switch traps to Area signals and add a demo level for testing.
    Converted the Trap class to use area_entered (detect hurtbox Area2D).
    Updated trap scenes and set global layer names and carried out testing in the demo level.

--- Scripts/Entities/Trap/arrow_trap.gd
+++ Scripts/Entities/Trap/arrow_trap.gd
@@ -1,5 +1,6 @@
 extends Trap
 
 func _on_enter_area_body_entered(body: Node2D) -> void:
+	print("Player entered ArrowTrap Region")
 	if body.has_method("take_damage"):
 		$"../AnimationPlayer".play("ArrowTrapAnimation")
--- Scripts/Entities/Trap/trap.gd
+++ Scripts/Entities/Trap/trap.gd
@@ -1,10 +1,11 @@
 class_name Trap extends Area2D
 
 @export var damage_per_hit: int = 1
+@export var knockback: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-	body_entered.connect(self._on_body_entered)
+	area_entered.connect(self._on_area_entered)
 	
-func _on_body_entered(body: Node2D):
-	if body.has_method("take_damage"):
-		body.take_damage(damage_per_hit)
+func _on_area_entered(area: Area2D):
+	if area.has_method("receive_hit"):
+		area.receive_hit(damage_per_hit, knockback, 0.0, owner)
